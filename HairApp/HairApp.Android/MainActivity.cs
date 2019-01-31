9c56beffbb7c87a09056da39b7d0c5bd7d14fddc
Hacky <hacky1610@gmx.de>
2019-01-31 22:25:37 +0100

    Fixes

--- HairApp/HairApp.Android/MainActivity.cs
+++ HairApp/HairApp.Android/MainActivity.cs
@@ -14,10 +14,6 @@ namespace HairApp.Droid
     [Activity(Label = "HairApp", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        static readonly int NOTIFICATION_ID = 1000;
-        static readonly string CHANNEL_ID = "location_notification";
-        internal static readonly string COUNT_KEY = "count";
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
@@ -28,7 +24,6 @@ namespace HairApp.Droid
             Rg.Plugins.Popup.Popup.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
-
             Remind(DateTime.Now, "Foo", "Bar");
         }
 
@@ -38,7 +33,6 @@ namespace HairApp.Droid
             Intent alarmIntent = new Intent(Application.Context, typeof(AlarmReceiver));
             alarmIntent.PutExtra("message", message);
             alarmIntent.PutExtra("title", title);
-            Calendar cur_cal = new GregorianCalendar();
             PendingIntent pendingIntent = PendingIntent.GetBroadcast(Application.Context, 0, alarmIntent, PendingIntentFlags.UpdateCurrent);
             AlarmManager alarmManager = (AlarmManager)Application.Context.GetSystemService(Context.AlarmService);
 
@@ -50,11 +44,12 @@ namespace HairApp.Droid
         {
             if (Rg.Plugins.Popup.Popup.SendBackPressed(base.OnBackPressed))
             {
-                // Do something if there are some pages in the `PopupStack`
+                HairApp.App.BL.Logger.WriteLine("Backpressed of Popup");
             }
             else
             {
-                // Do something if there are not any pages in the `PopupStack`
+                HairApp.App.BL.Logger.WriteLine("Normal Backpressed");
+
             }
         }
     }
