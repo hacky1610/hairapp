ee810cdeeff5961865d53caa2d969e5e7dc88ac9
Hacky <hacky1610@gmx.de>
2019-07-07 20:34:47 +0200

    new Job Logic

--- HairApp/HairApp.Android/AlarmJob.cs
+++ HairApp/HairApp.Android/AlarmJob.cs
@@ -6,10 +6,14 @@ namespace HairApp.Droid
     [Service(Exported = true, Name = "com.companyname.HairApp.Droid.AlarmJob", Permission = "android.permission.BIND_JOB_SERVICE")]
     public class AlarmJob:JobService
     {
-
         public override bool OnStartJob(JobParameters @params)
         {
             AndroidLog.WriteLog("Job started");
+            //Todo:Usa a variable for the language
+            var ci = new System.Globalization.CultureInfo("fr");
+            var loc = new UsingResxLocalization.Android.Localize();
+            loc.SetLocale(ci);
+
             var currentHour = DateTime.Now.Hour;
             if(currentHour >= 6)
             {
@@ -21,8 +25,6 @@ namespace HairApp.Droid
                 new ReminderReceiver().Notify(ApplicationContext);
             }
            
-
-           // JobUtil.scheduleJob(ApplicationContext); // reschedule the job
             return true;
         }
 
--- HairApp/HairApp.Android/JobUtil.cs
+++ HairApp/HairApp.Android/JobUtil.cs
@@ -1,29 +1,34 @@
 ﻿using Android.App.Job;
 using Android.Content;
+using System.Threading;
 
 namespace HairApp.Droid
 {
     class JobUtil
-
     {
         //https://www.vogella.com/tutorials/AndroidTaskScheduling/article.html
         //https://devblogs.microsoft.com/xamarin/replacing-services-jobs-android-oreo-8-0/
         public static void scheduleJob(Context context)
         {
+            for (int i = 1; i < 12; i++)
+            {
+                AndroidLog.WriteLog($"Create job {i}");
+
+                CreateJob(context, i);
+            }
+        }
+
+
+        private static void CreateJob(Context context,int minutesToWait)
+        {
             Java.Lang.Class javaClass = Java.Lang.Class.FromType(typeof(AlarmJob));
             ComponentName serviceComponent = new ComponentName(context, javaClass);
-            JobInfo.Builder builder = new JobInfo.Builder(0, serviceComponent);
-            //builder.SetMinimumLatency(10 * 60 * 1000); // wait at least
-            builder.SetPeriodic(JobInfo.MinPeriodMillis);
+            JobInfo.Builder builder = new JobInfo.Builder(minutesToWait, serviceComponent);
+            builder.SetPeriodic(JobInfo.MinPeriodMillis + 60000 * minutesToWait);
             builder.SetPersisted(true);
             builder.SetRequiredNetworkType(NetworkType.Any); // device should be idle
-            //builder.SetRequiresBatteryNotLow(false);
-            //builder.SetRequiresCharging(false);
-            //builder.SetRequiresDeviceIdle(false);
-            //builder.SetRequiresStorageNotLow(false);
             JobScheduler jobScheduler = (JobScheduler)context.GetSystemService(Context.JobSchedulerService);
             jobScheduler.Schedule(builder.Build());
-         
         }
 
 
